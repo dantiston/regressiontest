fca11575554a66cb2f9983af8193bdd4c34ae282
T.J. Trimble <dantiston@gmail.com>
2015-08-08 17:05:47 -0700

    CLI re-arranged a little bit, mostly working. Need to clean up help messages in respect to the subparser aliases

--- regressiontest/__main__.py
+++ regressiontest/__main__.py
@@ -41,57 +41,107 @@ def expandPattern(name):
     return result
 
 
+def getFirst(string):
+    return string[0]
+
+
 # Functions
-def run(filenames):
-    for filename in filenames:
-        regressiontest.runTest(filename)
+def run(args):
+    tests = expandPattern(args.pattern)
+    for test in tests:
+        regressiontest.runTest(test)
 
 
-def create(function, filenames):
+def create(args):
+    filenames = args.filenames
+    function = args.function
     for filename in filenames:
         regressiontest.createTest(function, filename)
 
 
-def update(filenames):
-    for filename in filenames:
-        regressiontest.updateTest(filename)
+def update(args):
+    tests = expandPattern(args.pattern)
+    for test in tests:
+        regressiontest.updateTest(test)
 
 
-def delete(filename):
-    if len(filename) > 1:
-        print("Deletion command only accepts one file at a time.",
+def delete(args):
+    tests = expandPattern(args.pattern)
+    if len(test) > 1:
+        print("Deletion command only accepts one fully-specified test at a time.",
               file=sys.stderr)
-    regressiontest.deleteTest(filename)
+        sys.exit(1)
+    regressiontest.deleteTest(test)
 
 
-def search(filenames):
-    print(", ".join(filenames))
+def search(args):
+    tests = expandPattern(args.pattern)
+    print(", ".join(tests))
 
 
-functionDict = {
-    "r":run,
-    "c":create,
-    "u":update,
-    "d":delete,
-    "s":search,
-}
 
-# Parse args
+# Argument parser
 parser = argparse.ArgumentParser(prog=constants.project,
                                  description='Create and run regression tests.')
-parser.add_argument("command", choices=functionDict.keys(), type=lambda x: x[0],
-                    help="command to process: one of {" + ",".join(functionDict.keys()) + "}")
-parser.add_argument("pattern", type=str,
-                    help="pattern matching names of regression tests to process")
 
 # Optional commands
-parser.add_argument('--version', action='version', version="%(prog)s " + constants.version)
+parser.add_argument('-v', '--version', action='version', version=" ".join(("%(prog)s", constants.version)))
 
-args = parser.parse_args()
 
-if args.command not in functionDict:
-    parser.print_usage()
-    sys.exit(1)
+# Subcommand parser
+subparsers = parser.add_subparsers(title="commands",
+                                   help='command to execute',
+                                   dest='command')
+subparsers.required = True # Make at least one of the subparsers required
+
+
+## Create subparser
+parser_create = subparsers.add_parser('c',
+                                      aliases=['create'],
+                                      help='Create a new regression test')
+parser_create.add_argument("function", type=str,
+                           help="function to test")
+parser_create.add_argument("filenames", type=str, nargs='+',
+                           help="filenames to ingest and create new tests from")
+parser_create.set_defaults(f=create)
+
+
+## Run subparser
+parser_run = subparsers.add_parser('r',
+                                   aliases=['run'],
+                                   help='Run the tests matching the specified pattern')
+parser_run.add_argument("pattern", type=str,
+                        help="pattern matching names of regression tests to run")
+parser_run.set_defaults(f=run)
+
+
+## Update subparser
+parser_update = subparsers.add_parser('u',
+                                      aliases=['update'],
+                                      help='Update the tests matching the specified pattern')
+parser_update.add_argument("pattern", type=str,
+                           help="pattern matching names of regression tests to update")
+parser_update.set_defaults(f=update)
+
+
+## Delete subparser
+parser_delete = subparsers.add_parser('d',
+                                      aliases=['delete'],
+                                      help='Delete the test specified')
+parser_delete.add_argument("name", type=str,
+                           help="name of test to delete")
+parser_delete.set_defaults(f=delete)
+
+
+## Search subparser
+parser_search = subparsers.add_parser('s',
+                                      aliases=['search'],
+                                      help='Search for tests matching the pattern')
+parser_search.add_argument("pattern", type=str,
+                           help="pattern to search for")
+parser_search.set_defaults(f=search)
+
 
 # Execute command
-functionDict[args.command](expandPattern(args.pattern))
+args = parser.parse_args()
+args.f(args)
